341d20be52e3b4156118580238e43d8b3244f4a1
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-10-25 14:40:00 -0500

    Moved password to the git ignore file

--- SqlIntro/Program.cs
+++ SqlIntro/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using MySql.Data.MySqlClient;
@@ -9,42 +10,46 @@ namespace SqlIntro
     {
         static void Main(string[] args)
         {
-            var connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=jaonna111;"; 
-            var repo = new ProductRepository(new MySqlConnection(connectionString));
-            foreach (var prod in repo.GetProducts())
+            var connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
+            using (var conn = new MySqlConnection(connectionString))
             {
-                Console.WriteLine
-                    (prod.ProductId + " " + prod.Name +   " " + prod.Color);
+                conn.Open();
+
+                var repo = new ProductRepository(conn);
+                foreach (var prod in repo.GetProducts())
+                {
+                    Console.WriteLine
+                        (prod.ProductId + " " + prod.Name + " " + prod.Color);
+                }
+
+                var product = new Product
+                {
+                    ProductId = 5,
+                    Name = "Bike Rack",
+                    ProductNumber = "BR-1004",
+                    Color = "Blue",
+                    SafetyStockLevel = 1000,
+                    ReorderPoint = 500,
+                    StandardCost = 500.75,
+                    ListPrice = 1025.36,
+                    ModifiedDate = DateTime.Now
+                };
+
+                repo.InsertProduct(product);
+
+                var oProduct = new Product()
+                {
+                    ProductId = 1,
+                    Name = "Road Black, 52"
+                };
+
+                repo.UpdateProduct(oProduct);
+
+                repo.DeleteProduct(998);
+
+                Console.ReadLine();
             }
-
-            var product = new Product
-            {
-                ProductId = 5,
-                Name = "Bike Rack",
-                ProductNumber = "BR-1004",
-                Color = "Blue",
-                SafetyStockLevel = 1000,
-                ReorderPoint = 500,
-                StandardCost = 500.75,
-                ListPrice = 1025.36,
-                ModifiedDate = DateTime.Now
-            };
-
-            repo.InsertProduct(product);
-
-            var oProduct = new Product()
-            {
-                ProductId = 1,
-                Name = "Road Black, 52"
-            };
-
-            repo.UpdateProduct(oProduct);
-
-            repo.DeleteProduct(998);
-           
-            Console.ReadLine();
         }
-
-       
     }
 }
